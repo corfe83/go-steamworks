6ca2a75a74610906006c0e058caf820595a5c7e6
Michael Carlson <corfe83@gmail.com>
2025-12-07 16:19:16 -0800

    Go fmt and fix a bug with UGC field in unix due to struct alignment

--- leaderboards_packlarge.go
+++ leaderboards_packlarge.go
@@ -7,31 +7,35 @@ package steamworks
 type LeaderboardFindResult_t struct {
 	SteamLeaderboard SteamLeaderboard_t
 	LeaderboardFound byte
-	_ [7]byte // padding
+	_                [7]byte // padding
 }
 
 type LeaderboardScoresDownloaded_t struct {
-	SteamLeaderboard SteamLeaderboard_t
+	SteamLeaderboard        SteamLeaderboard_t
 	SteamLeaderboardEntries SteamLeaderboardEntries_t
-	EntryCount int32
-	_ [4]byte // padding
+	EntryCount              int32
+	_                       [4]byte // padding
 }
 
 type LeaderboardScoreUploaded_t struct {
-	Success                  byte
-	_                        [7]byte // padding
-	SteamLeaderboard         SteamLeaderboard_t
-	Score                    int32
-	ScoreChanged             byte
-	_                        [3]byte // padding
-	GlobalRankNew            int32
-	GlobalRankPrevious       int32
-	_                        [4]byte // padding
+	Success            byte
+	_                  [7]byte // padding
+	SteamLeaderboard   SteamLeaderboard_t
+	Score              int32
+	ScoreChanged       byte
+	_                  [3]byte // padding
+	GlobalRankNew      int32
+	GlobalRankPrevious int32
+	_                  [4]byte // padding
 }
 type LeaderboardEntry_t struct {
 	SteamIDUser CSteamID
 	GlobalRank  int32
 	Score       int32
 	Details     int32
-	UGC         UGCHandle_t
+	ugc         UGCHandle_t
+}
+
+func (me LeaderboardEntry_t) UGC() UGCHandle_t {
+	return me.ugc
 }
--- leaderboards_packsmall.go
+++ leaderboards_packsmall.go
@@ -2,29 +2,31 @@
 
 package steamworks
 
+import "unsafe"
+
 // Non-Windows platforms (Unix, macOS, FreeBSD) use VALVE_CALLBACK_PACK_SMALL as defined in steamclientpublic.h (4-byte alignment)
 
 type LeaderboardFindResult_t struct {
 	SteamLeaderboard SteamLeaderboard_t
 	LeaderboardFound byte
-	_ [3]byte // padding
+	_                [3]byte // padding
 }
 
 type LeaderboardScoresDownloaded_t struct {
-	SteamLeaderboard SteamLeaderboard_t
+	SteamLeaderboard        SteamLeaderboard_t
 	SteamLeaderboardEntries SteamLeaderboardEntries_t
-	EntryCount int32
+	EntryCount              int32
 }
 
 type LeaderboardScoreUploaded_t struct {
-	Success                  byte
-	_                        [3]byte // padding
-	SteamLeaderboard         SteamLeaderboard_t
-	Core                     int32
-	ScoreChanged             byte
-	_                        [3]byte // padding
-	GlobalRankNew            int32
-	GlobalRankPrevious       int32
+	Success            byte
+	_                  [3]byte // padding
+	SteamLeaderboard   SteamLeaderboard_t
+	Core               int32
+	ScoreChanged       byte
+	_                  [3]byte // padding
+	GlobalRankNew      int32
+	GlobalRankPrevious int32
 }
 
 type LeaderboardEntry_t struct {
@@ -32,5 +34,10 @@ type LeaderboardEntry_t struct {
 	GlobalRank  int32
 	Score       int32
 	Details     int32
-	UGC         UGCHandle_t
+	ugc         [8]byte // if we use the actual UGCHandle_t here, struct size/alignment changes
+}
+
+func (entry LeaderboardEntry_t) UGC() UGCHandle_t {
+	ugcPtr := unsafe.Pointer(&entry.ugc[0])
+	return *(*UGCHandle_t)(ugcPtr)
 }
